3d0acceb18dd90240107de75e475cdf965dface4
MenkeTechnologies <linux.dev25@gmail.com>
2020-08-11 00:39:09 -0400

    add missing locals

--- zsh-sed-sub.plugin.zsh
+++ zsh-sed-sub.plugin.zsh
@@ -1,5 +1,8 @@
 basicSedSub(){
     emulate -LR zsh
+
+    local sedArg key orig replace
+
     print -r "$BUFFER" | command egrep -q '\w+' || {
         zle up-line-or-history
         #printf "\x1b[1;31m"
@@ -11,49 +14,49 @@ basicSedSub(){
     printf "\x1b[1;34m"
     zle -R "Extended Regex Sed Substitution (original>replaced) (@ not allowed in either string):"
     printf "\x1b[1;44;37m"
-    local SEDARG=""
-    local key=""
     read -k key
     local -r start=$key
+
     while (( (#key)!=(##\n) && (#key)!=(##\r) )) ; do
         if (( (#key)==(##\>) ));then
             printf "\x1b[0;4;1;34m"
         else
             printf "\x1b[1;44;37m"
-            print -r "$SEDARG" | grep -q '>' && printf \
+            print -r "$sedArg" | grep -q '>' && printf \
                 "\x1b[0;1;37;45m"
         fi
         if (( (#key)==(##^?) || (#key)==(##^h) ));then
-            SEDARG=${SEDARG[1,-2]}
+            sedArg=${sedArg[1,-2]}
             printf "\x1b[0m"
         elif (( (#key)==(##^U) ));then
-            SEDARG=""
+            sedArg=""
             printf "\x1b[0m"
         else
             if (( (#key)!=(##@) ));then
-                SEDARG="${SEDARG}$key"
+                sedArg="${sedArg}$key"
             fi
         fi
-        zle -R "Extended Regex Sed Substitution (original>replaced) (@ not allowed in either string): $SEDARG"
+        zle -R "Extended Regex Sed Substitution (original>replaced) (@ not allowed in either string): $sedArg"
         read -k key || return 1
     done
-    print -r "$SEDARG" | grep -q "@" && {
+
+    if print -r "$sedArg" | grep -q "@"; then
         printf "\x1b[0;1;31m"
         zle -R "No '@' allowed! That is the sed delimiter!" && read -k key
-    printf "\x1b[0m"
-    return 1
-}
+        printf "\x1b[0m"
+        return 1
+    fi
 
-    print -r "$SEDARG" | grep -q ">" || {
+    print -r "$sedArg" | grep -q ">" || {
         printf "\x1b[0;1;31m"
         zle -R  "Needed '>' for separation of original regex string and substitution!" && read -k 1
         printf "\x1b[0m"
         return 1
     }
 
-    orig="$(print -r $SEDARG | awk -F'>' '{print $1}')"
-    replace="$(print -r $SEDARG | awk -F'>' '{print $2}')"
-    SEDARG="s@$orig@$replace@g"
+    orig="$(print -r $sedArg | awk -F'>' '{print $1}')"
+    replace="$(print -r $sedArg | awk -F'>' '{print $2}')"
+    sedArg="s@$orig@$replace@g"
 
     print -r "$BUFFER" | command egrep -q "$orig" || {
         printf "\x1b[0;1;31m"
@@ -62,7 +65,7 @@ basicSedSub(){
         return 1
     }
 
-    BUFFER="$(print -r $BUFFER | sed -E "$SEDARG")"
+    BUFFER="$(print -r $BUFFER | sed -E "$sedArg")"
     printf "\x1b[0m"
 }
 
